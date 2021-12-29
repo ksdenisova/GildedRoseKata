06d54432dac142690e9ea9148cff43c70c8a3a79
Kseniia Denisova <kdenisova@8thlight.com>
2021-12-29 16:04:09 -0500

    Refactor update_item for regular item and Aged Brie

--- elixir/lib/gilded_rose.ex
+++ elixir/lib/gilded_rose.ex
@@ -1,58 +1,47 @@
 defmodule GildedRose do
-  alias Item.Manager
+  alias Item.Modifier
+
   alias Item.Item
 
-  def update_quality(items) do
-    Enum.map(items, &update_item/1)
-  end
+  def update_quality(items), do: Enum.map(items, &update_item/1)
 
   def update_item(%Item{name: "Sulfuras, Hand of Ragnaros"} = item), do: item
 
-  def update_item(%Item{quality: quality} = item) when quality >= 50, do: Manager.decrease_sell_in(item)
+  def update_item(%Item{quality: quality} = item) when quality >= 50, do: Modifier.decrease_sell_in(item)
 
   def update_item(%Item{name: "Backstage passes to a TAFKAL80ETC concert"} = item) do
-    item = Manager.increase_quality(item)
+    item = Modifier.increase_quality(item)
 
     item = cond do
             item.sell_in <= 10 ->
-              Manager.increase_quality(item)
+              Modifier.increase_quality(item)
             true -> item
           end
 
     item = cond do
             item.sell_in < 6 ->
-              Manager.increase_quality(item)
+              Modifier.increase_quality(item)
             true -> item
           end
 
-    item = Manager.decrease_sell_in(item)
+    item = Modifier.decrease_sell_in(item)
 
     item = cond do
             item.sell_in < 0 ->
-              Manager.drop_quality(item)
+              Modifier.drop_quality(item)
             true -> item
           end
   end
 
   def update_item(%Item{name: "Aged Brie"} = item) do
-    item = Manager.increase_quality(item)
-            |> Manager.decrease_sell_in
-
-    cond do
-      item.sell_in < 0 && item.quality < 50 ->
-        Manager.increase_quality(item)
-      true -> item
-    end
+    item
+    |> Modifier.increase_quality()
+    |> Modifier.decrease_sell_in()
   end
 
   def update_item(%Item{} = item) do
-    item = Manager.decrease_quality(item)
-            |> Manager.decrease_sell_in()
-
-    cond do
-      item.sell_in < 0 ->
-        Manager.decrease_quality(item)
-      true -> item
-    end
+    item
+    |> Modifier.decrease_quality()
+    |> Modifier.decrease_sell_in()
   end
 end
--- elixir/lib/item/modifier.ex
+++ elixir/lib/item/modifier.ex
@@ -1,5 +1,9 @@
-defmodule Item.Manager do
+defmodule Item.Modifier do
   alias Item.Item
+
+  def increase_quality(%Item{sell_in: sell_in, quality: quality} = item) when sell_in < 0 and quality < 48 do
+    %Item{item | quality: quality + 2}
+  end
 
   def increase_quality(%Item{quality: quality} = item) when quality < 50 do
     %Item{item | quality: quality + 1}
@@ -7,8 +11,12 @@ defmodule Item.Manager do
 
   def increase_quality(%Item{} = item), do: item
 
+  def decrease_quality(%Item{quality: quality, sell_in: sell_in} = item) when quality >= 2 and sell_in <= 0 do
+      %Item{item | quality: quality - 2}
+  end
+
   def decrease_quality(%Item{quality: quality} = item) when quality > 0 do
-    %Item{item | quality: quality - 1}
+      %Item{item | quality: quality - 1}
   end
 
   def decrease_quality(%Item{} = item), do: item
