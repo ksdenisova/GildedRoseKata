b8f8113eddd7c085896e294d94e733930a913058
Kseniia Denisova <kdenisova@8thlight.com>
2021-12-29 10:27:11 -0500

    Credo checks fix

--- elixir/test/gilded_rose_test.exs
+++ elixir/test/gilded_rose_test.exs
@@ -12,16 +12,16 @@ defmodule GildedRoseTest do
   test "decreases sell in days by one" do
     item = %Item{name: "test_item", sell_in: 5, quality: 10}
             |> GildedRose.update_item
-    
+
     expected = 4
 
     assert item.sell_in == expected
   end
-  
+
   test "decreases quality by one for regular items" do
     item = %Item{name: "test_item", sell_in: 5, quality: 10}
             |> GildedRose.update_item
-    
+
     expected = 9
 
     assert item.quality == expected
@@ -30,7 +30,7 @@ defmodule GildedRoseTest do
   test "decreases quality by two for regular items if sell by date has passed" do
     item = %Item{name: "test_item", sell_in: 0, quality: 10}
             |> GildedRose.update_item
-    
+
     expected = 8
 
     assert item.quality == expected
@@ -55,7 +55,7 @@ defmodule GildedRoseTest do
     quality = 15
     item = %Item{name: "Aged Brie", sell_in: 5, quality: quality}
             |> GildedRose.update_item
-    
+
     expected = quality + 1
 
     assert item.quality == expected
@@ -65,7 +65,7 @@ defmodule GildedRoseTest do
     quality = 15
     item = %Item{name: "Aged Brie", sell_in: -5, quality: quality}
             |> GildedRose.update_item
-    
+
     expected = quality + 2
 
     assert item.quality == expected
@@ -90,7 +90,7 @@ defmodule GildedRoseTest do
     quality = 50
     item = %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: 5, quality: quality}
             |> GildedRose.update_item
-    
+
     expected = quality
 
     assert item.quality == expected
@@ -100,7 +100,7 @@ defmodule GildedRoseTest do
     sell_in = 5
     item = %Item{name: "Sulfuras, Hand of Ragnaros", sell_in: sell_in, quality: 16}
             |> GildedRose.update_item
-    
+
     expected = sell_in
 
     assert item.sell_in == expected
@@ -110,7 +110,7 @@ defmodule GildedRoseTest do
     quality = 16
     item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 11, quality: quality}
             |> GildedRose.update_item
-    
+
     expected = quality + 1
 
     assert item.quality == expected
@@ -149,7 +149,7 @@ defmodule GildedRoseTest do
     test "the quality of item 'Backstage passes...' drops to 0 after sell in days passed" do
     item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 0, quality: 15}
             |> GildedRose.update_item
-    
+
     expected = 0
 
     assert item.quality == expected
@@ -158,7 +158,7 @@ defmodule GildedRoseTest do
   test "the quality of item 'Backstage passes...' does not increase after it reaches 50" do
     item = %Item{name: "Backstage passes to a TAFKAL80ETC concert", sell_in: 5, quality: 50}
             |> GildedRose.update_item
-    
+
     expected = 50
 
     assert item.quality == expected
